8e603e3aec7e541b5b33d9d23066d1fd9f82de32
Rishu <richrishu69@gmail.com>
2026-05-27 13:41:31 +0530

    Added edge case for no input

--- todolist/script.js
+++ todolist/script.js
@@ -1,13 +1,27 @@
 function addTask(){
+
+    const input = document.getElementById('inputTask');
+    const taskText = input.value.trim();
+
+    if(taskText === ""){
+        alert("Bro! give some input 🥲");
+        return;
+    }
+
     const newTask = document.createElement('li');
     const taskList = document.getElementById('taskList');
-    newTask.textContent = document.getElementById('inputTask').value;
+
+    newTask.textContent = taskText;
     deleteTask(newTask);
     taskList.appendChild(newTask);
     document.getElementById('inputTask').value = "";
     
 }
 function deleteTask(newTask){
+
+    if(taskText === ""){
+        alert("Bro! give some input🥲")
+    }
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = "Delete";
     newTask.appendChild(deleteBtn);
