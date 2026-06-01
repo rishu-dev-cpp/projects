f82c08e24b8184f244dc8b45dd1acf60c312b816
Rishu <richrishu69@gmail.com>
2026-06-01 17:41:07 +0530

    New Project to find user from github using there username where i used api of github

---
+++ findprofile/script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('find');
+const username = document.getElementById('profile-name');
+const img = document.getElementById('profile-img');
+
+button.addEventListener('click', () => {
+    const input = document.getElementById('search');
+    let userinput = input.value;
+    
+    const requestUrl = `https://api.github.com/users/${userinput}`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', requestUrl);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            const data = JSON.parse(this.responseText);
+            img.src = data.avatar_url;
+            username.textContent = data.name;
+        }
+    }
+    xhr.send();
+    input.value = "";
+});
