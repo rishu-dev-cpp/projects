f7fbddc19384831c1433c8d71d52de17a58aea9b
Rishu <richrishu69@gmail.com>
2026-06-02 11:37:33 +0530

    used new method async await to fetch api

--- findprofile/script.js
+++ findprofile/script.js
@@ -1,6 +1,9 @@
 const button = document.getElementById('find');
 const username = document.getElementById('profile-name');
 const img = document.getElementById('profile-img');
+/*
+
+Old method using AJAX
 
 button.addEventListener('click', () => {
     const input = document.getElementById('search');
@@ -18,5 +21,34 @@ button.addEventListener('click', () => {
         }
     }
     xhr.send();
+    input.value = "";
+});
+*/
+
+// New method
+button.addEventListener('click', async () => {
+    const input = document.getElementById('search');
+    let userinput = input.value;
+    
+    const requestUrl = `https://api.github.com/users/${userinput}`;
+
+    try {
+        const response = await fetch(requestUrl);
+        
+        if (!response.ok) {
+            alert("Sorry! Didn't got the DATA");
+            return;
+        }
+
+        const data = await response.json();
+        
+        img.src = data.avatar_url;
+        username.textContent = data.name || "No Name Provided";
+        img.style.display = "block"; 
+
+    } catch (error) {
+        console.error("Error occured:", error);
+    }
+
     input.value = "";
 });
